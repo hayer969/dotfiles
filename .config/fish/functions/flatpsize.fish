566c6c3c9b512116b1b4511e92cb2513de3b3904
hayer969 <hayerdp@gmail.com>
2022-12-13 23:56:27 +0300

    fix if have multibranches

--- .config/fish/functions/flatpsize.fish
+++ .config/fish/functions/flatpsize.fish
@@ -1,6 +1,6 @@
 # Calculate overall used space by flatpak
 function flatpsize -d "Calculate overall used space by flatpak"
-    echo (math (for x in (flatpak list | cut -f2)
+    echo (math (for x in (flatpak list | cut -f2,3,4 | sed "s/\t/\//g" | sed -e "s|/.*/|//|")
               echo +(flatpak info -s $x)
           end | math) / 1000000000) Gb
 end
