a1d85366d62c5727a4a938de52e560c9af04efae
hayer969 <hayerdp@gmail.com>
2024-02-23 15:31:51 +0300

    Now can use console when debug python with dap ui

--- .config/nvim/lua/user/dap.lua
+++ .config/nvim/lua/user/dap.lua
@@ -1,73 +1,74 @@
 local M = {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"mfussenegger/nvim-dap-python",
-		"rcarriga/nvim-dap-ui",
-	},
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "mfussenegger/nvim-dap-python",
+        "rcarriga/nvim-dap-ui",
+    },
 }
 
 function M.config()
-	local dap = require("dap")
-	local dapui = require("dapui")
-	local dap_python = require("dap-python")
+    local dap = require("dap")
+    local dapui = require("dapui")
+    local dap_python = require("dap-python")
 
-	dap_python.setup("/home/hayer/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
-	table.insert(dap.configurations.python, 1, {
-		type = "python",
-		request = "launch",
-		name = "Launch with Not Just My Code",
-		program = "${file}",
-		justMyCode = false,
-	})
+    dap_python.setup("/home/hayer/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
+    table.insert(dap.configurations.python, 1, {
+        type = "python",
+        request = "launch",
+        name = "Launch with Not Just My Code",
+        program = "${file}",
+        console = "integratedTerminal",
+        justMyCode = false,
+    })
 
-	require("dap").adapters["pwa-node"] = {
-		type = "server",
-		host = "localhost",
-		port = "${port}",
-		executable = {
-			command = "node",
-			-- 💀 Make sure to update this path to point to your installation
-			args = {
-				"/home/hayer/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
-				"${port}",
-			},
-		},
-	}
+    require("dap").adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+            command = "node",
+            -- 💀 Make sure to update this path to point to your installation
+            args = {
+                "/home/hayer/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+                "${port}",
+            },
+        },
+    }
 
-	for _, language in ipairs({ "typescript", "javascript" }) do
-		require("dap").configurations[language] = {
-			{
-				type = "pwa-node",
-				request = "launch",
-				name = "Launch file",
-				program = "${file}",
-				cwd = "${workspaceFolder}",
-			},
-			{
-				type = "pwa-node",
-				request = "attach",
-				name = "Attach",
-				processId = require("dap.utils").pick_process,
-				cwd = "${workspaceFolder}",
-			},
-		}
-	end
+    for _, language in ipairs({ "typescript", "javascript" }) do
+        require("dap").configurations[language] = {
+            {
+                type = "pwa-node",
+                request = "launch",
+                name = "Launch file",
+                program = "${file}",
+                cwd = "${workspaceFolder}",
+            },
+            {
+                type = "pwa-node",
+                request = "attach",
+                name = "Attach",
+                processId = require("dap.utils").pick_process,
+                cwd = "${workspaceFolder}",
+            },
+        }
+    end
 
-	dapui.setup()
+    dapui.setup()
 
-	vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 
-	dap.listeners.after.event_initialized["dapui_config"] = function()
-		dapui.open()
-	end
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+    end
 
-	dap.listeners.before.event_terminated["dapui_config"] = function()
-		dapui.close()
-	end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+    end
 
-	dap.listeners.before.event_exited["dapui_config"] = function()
-		dapui.close()
-	end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+    end
 end
 
 return M
