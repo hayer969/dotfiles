d5cb62bd896570bf71f8567252939d5c2624969a
hayer969 <hayerdp@gmail.com>
2024-08-10 18:58:16 +0300

    Update nvim

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -49,33 +49,33 @@ function M.config()
         return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
     end
 
-    local kind_icons = {
-        Text = "",
-        Method = "",
-        Function = "",
-        Constructor = "",
-        Field = "",
-        Variable = "",
-        Class = "",
-        Interface = "",
-        Module = "",
-        Property = "",
-        Unit = "",
-        Value = "",
-        Enum = "",
-        Keyword = "",
-        Snippet = "",
-        Color = "",
-        File = "",
-        Reference = "",
-        Folder = "",
-        EnumMember = "",
-        Constant = "",
-        Struct = "",
-        Event = "",
-        Operator = "",
-        TypeParameter = "",
-    }
+    -- local kind_icons = {
+    --     Text = "",
+    --     Method = "",
+    --     Function = "",
+    --     Constructor = "",
+    --     Field = "",
+    --     Variable = "",
+    --     Class = "",
+    --     Interface = "",
+    --     Module = "",
+    --     Property = "",
+    --     Unit = "",
+    --     Value = "",
+    --     Enum = "",
+    --     Keyword = "",
+    --     Snippet = "",
+    --     Color = "",
+    --     File = "",
+    --     Reference = "",
+    --     Folder = "",
+    --     EnumMember = "",
+    --     Constant = "",
+    --     Struct = "",
+    --     Event = "",
+    --     Operator = "",
+    --     TypeParameter = "",
+    -- }
 
     cmp.setup({
         snippet = {
@@ -130,7 +130,7 @@ function M.config()
         formatting = {
             fields = { "kind", "abbr", "menu" },
             format = function(entry, vim_item)
-                vim_item.kind = kind_icons[vim_item.kind]
+                -- vim_item.kind = kind_icons[vim_item.kind]
                 vim_item.menu = ({
                     nvim_lsp = "",
                     nvim_lua = "",
--- .config/nvim/lua/user/whichkey.lua
+++ .config/nvim/lua/user/whichkey.lua
@@ -6,6 +6,11 @@ function M.config()
     local which_key = require("which-key")
 
     local setup = {
+        preset = "modern",
+        delay = function(ctx)
+            return ctx.plugin and 0 or 400
+        end,
+        --- You can add any mappings here, or use `req
         plugins = {
             marks = true, -- shows a list of your marks on ' and `
             registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -16,7 +21,7 @@ function M.config()
             -- the presets plugin, adds help for a bunch of default keybindings in Neovim
             -- No actual key bindings are created
             presets = {
-                operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+                operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
                 motions = true, -- adds help for motions
                 text_objects = true, -- help for text objects triggered after entering an operator
                 windows = true, -- default bindings on <c-w>
@@ -25,195 +30,150 @@ function M.config()
                 g = true, -- bindings for prefixed with g
             },
         },
-        -- add operators that will trigger motion and text object completion
-        -- to enable all native operators, set the preset / operators plugin above
-        -- operators = { gc = "Comments" },
-        key_labels = {
-            -- override the label used to display some keys. It doesn't effect WK in any other way.
-            -- For example:
-            -- ["<space>"] = "SPC",
-            -- ["<cr>"] = "RET",
-            -- ["<tab>"] = "TAB",
-        },
         icons = {
             breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
             separator = "➜", -- symbol used between a key and it's label
             group = "+", -- symbol prepended to a group
         },
-        popup_mappings = {
-            scroll_down = "<c-d>", -- binding to scroll down inside the popup
-            scroll_up = "<c-u>", -- binding to scroll up inside the popup
-        },
-        window = {
-            border = "rounded", -- none, single, double, shadow
-            position = "bottom", -- bottom, top
-            margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-            padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-            winblend = 0,
-        },
         layout = {
-            height = { min = 4, max = 25 },                                     -- min and max height of the columns
-            width = { min = 20, max = 50 },                                     -- min and max width of the columns
-            spacing = 3,                                                        -- spacing between columns
-            align = "left",                                                     -- align columns left, center or right
+            height = { min = 4, max = 25 }, -- min and max height of the columns
+            width = { min = 20, max = 50 }, -- min and max width of the columns
+            spacing = 3,           -- spacing between columns
+            align = "left",        -- align columns left, center or right
         },
-        ignore_missing = true,                                                  -- enable this to hide mappings for which you didn't specify a label
-        hidden = { "<silent>", "<CMD>", "<CMD>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-        show_help = true,                                                       -- show help message on the command line when the popup is visible
-        triggers = "auto",                                                      -- automatically setup triggers
-        -- triggers = {"<leader>"} -- or specify a list manually
-        triggers_blacklist = {
-            -- list of mode / prefixes that should never be hooked by WhichKey
-            -- this is mostly relevant for key maps that start with a native binding
-            -- most people should not need to change this
-            i = { "j", "k" },
-            v = { "j", "k" },
-        },
+        show_help = true,          -- show help message on the command line when the popup is visible
+        disable = { bt = { "terminal" } },
     }
 
-    local opts = {
-        mode = "n", -- NORMAL mode
-        prefix = "<leader>",
-        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-        silent = true, -- use `silent` when creating keymaps
-        noremap = true, -- use `noremap` when creating keymaps
-        nowait = true, -- use `nowait` when creating keymaps
-    }
-
-    local mappings = {
-        ["t"] = { "Open buffer in new tab" },
-        ["c"] = { "Change word and find next match" },
-        [";"] = { "Clear highlights" },
-        ["r"] = { "Run ipython cell like in VSCode" },
-        ["%"] = { "Put absolute full path" },
-        ["/"] = { "Fuzzily search in current buffer" },
-
-        f = {
-            name = "Find",
-            t = { "Find text in files" },
-            T = { "Find Telescope builtins" },
-            s = { "Find string in files" },
-            b = { "Find files in buffers" },
-            f = { "Find files" },
-            r = { "Open Recent File" },
-            p = { "Find projects" },
-            R = { "Registers" },
-            h = { "Find in command history" },
-            H = { "Find in HELP" },
-            k = { "Keymaps" },
-            C = { "Commands" },
-            m = { "Harpoon marks" },
-            w = {
-                name = "Workspace features",
-                s = { "Find lsp Workspace symbols" },
+    which_key.add({
+        {
+            mode = { "n" },
+            { "<leader>%",   desc = "Put absolute full path",           nowait = true, remap = false },
+            { "<leader>/",   desc = "Fuzzily search in current buffer", nowait = true, remap = false },
+            { "<leader>;",   desc = "Clear highlights",                 nowait = true, remap = false },
+            { "<leader>c",   desc = "Change word and find next match",  nowait = true, remap = false },
+            { "<leader>d",   group = "DAP",                             nowait = true, remap = false },
+            { "<leader>dB",  desc = "Conditional breakpoint",           nowait = true, remap = false },
+            { "<leader>dH",  desc = "Hit condition breakpoint",         nowait = true, remap = false },
+            { "<leader>dL",  desc = "Log point",                        nowait = true, remap = false },
+            { "<leader>dO",  desc = "Step out <F4>",                    nowait = true, remap = false },
+            { "<leader>db",  desc = "Toggle breakpoint",                nowait = true, remap = false },
+            { "<leader>dc",  desc = "Continue <F5>",                    nowait = true, remap = false },
+            { "<leader>di",  desc = "Step into <F2>",                   nowait = true, remap = false },
+            { "<leader>dl",  desc = "Run last",                         nowait = true, remap = false },
+            { "<leader>do",  desc = "Step over <F3>",                   nowait = true, remap = false },
+            { "<leader>dq",  desc = "Quit (terminate) session",         nowait = true, remap = false },
+            { "<leader>dr",  desc = "Toggle repl",                      nowait = true, remap = false },
+            { "<leader>ds",  desc = "Debug selected",                   nowait = true, remap = false },
+            { "<leader>dt",  group = "Debug tests",                     nowait = true, remap = false },
+            { "<leader>dtc", desc = "Class under cursor",               nowait = true, remap = false },
+            { "<leader>dtm", desc = "Method under cursor",              nowait = true, remap = false },
+            { "<leader>du",  desc = "Toggle UI",                        nowait = true, remap = false },
+            { "<leader>f",   group = "Find",                            nowait = true, remap = false },
+            { "<leader>f@",  desc = "Find lsp document symbols",        nowait = true, remap = false },
+            { "<leader>fC",  desc = "Commands",                         nowait = true, remap = false },
+            { "<leader>fH",  desc = "Find in HELP",                     nowait = true, remap = false },
+            { "<leader>fR",  desc = "Registers",                        nowait = true, remap = false },
+            { "<leader>fT",  desc = "Find Telescope builtins",          nowait = true, remap = false },
+            { "<leader>fb",  desc = "Find files in buffers",            nowait = true, remap = false },
+            { "<leader>ff",  desc = "Find files",                       nowait = true, remap = false },
+            { "<leader>fh",  desc = "Find in command history",          nowait = true, remap = false },
+            { "<leader>fk",  desc = "Keymaps",                          nowait = true, remap = false },
+            { "<leader>fm",  desc = "Harpoon marks",                    nowait = true, remap = false },
+            { "<leader>fp",  desc = "Find projects",                    nowait = true, remap = false },
+            { "<leader>fr",  desc = "Open Recent File",                 nowait = true, remap = false },
+            { "<leader>fs",  desc = "Find string in files",             nowait = true, remap = false },
+            { "<leader>ft",  desc = "Find text in files",               nowait = true, remap = false },
+            { "<leader>fw",  group = "Workspace features",              nowait = true, remap = false },
+            { "<leader>fws", desc = "Find lsp Workspace symbols",       nowait = true, remap = false },
+            { "<leader>g",   group = "Git",                             nowait = true, remap = false },
+            { "<leader>gR",  desc = "Reset Buffer",                     nowait = true, remap = false },
+            { "<leader>gb",  desc = "Checkout branch",                  nowait = true, remap = false },
+            { "<leader>gc",  desc = "Checkout commit",                  nowait = true, remap = false },
+            { "<leader>gd",  desc = "Diff",                             nowait = true, remap = false },
+            { "<leader>gg",  desc = "Lazygit",                          nowait = true, remap = false },
+            { "<leader>gj",  desc = "Next Hunk",                        nowait = true, remap = false },
+            { "<leader>gk",  desc = "Prev Hunk",                        nowait = true, remap = false },
+            { "<leader>gl",  desc = "Blame",                            nowait = true, remap = false },
+            { "<leader>go",  desc = "Open changed file",                nowait = true, remap = false },
+            { "<leader>gp",  desc = "Preview Hunk",                     nowait = true, remap = false },
+            { "<leader>gr",  desc = "Reset Hunk",                       nowait = true, remap = false },
+            { "<leader>gs",  desc = "Stage Hunk",                       nowait = true, remap = false },
+            { "<leader>gu",  desc = "Undo Stage Hunk",                  nowait = true, remap = false },
+            { "<leader>h",   group = "Harpoon",                         nowait = true, remap = false },
+            { "<leader>hc",  desc = "Open term commands quick menu",    nowait = true, remap = false },
+            { "<leader>hm",  desc = "Set marks",                        nowait = true, remap = false },
+            { "<leader>hn",  desc = "Go to the next mark",              nowait = true, remap = false },
+            { "<leader>hp",  desc = "Go to the previous mark",          nowait = true, remap = false },
+            { "<leader>hu",  desc = "Open files quick menu",            nowait = true, remap = false },
+            { "<leader>l",   group = "LSP",                             nowait = true, remap = false },
+            {
+                "<leader>la",
+                "<CMD>lua vim.lsp.buf.code_action()<CR>",
+                desc = "Code action",
+                nowait = true,
+                remap = false,
             },
-            ["@"] = { "Find lsp document symbols" },
+            {
+                "<leader>lf",
+                "<CMD>lua vim.lsp.buf.format({async=true})<CR>",
+                desc = "Formatting code",
+                nowait = true,
+                remap = false,
+            },
+            { "<leader>li", "<CMD>LspInfo<CR>",                  desc = "Lsp Info", nowait = true, remap = false },
+            {
+                "<leader>lj",
+                "<CMD>lua vim.diagnostic.goto_next({buffer=0})<CR>",
+                desc = "Go to next diagnostic",
+                nowait = true,
+                remap = false,
+            },
+            {
+                "<leader>lk",
+                "<CMD>lua vim.diagnostic.goto_prev({buffer=0})<CR>",
+                desc = "Go to prev diagnostic",
+                nowait = true,
+                remap = false,
+            },
+            {
+                "<leader>lq",
+                "<CMD>lua vim.diagnostic.setloclist()<CR>",
+                desc = "Diagnostic list",
+                nowait = true,
+                remap = false,
+            },
+            { "<leader>lr", "<CMD>lua vim.lsp.buf.rename()<CR>", desc = "Rename",   nowait = true, remap = false },
+            {
+                "<leader>ls",
+                "<CMD>lua vim.lsp.buf.signature_help()<CR>",
+                desc = "Signature help",
+                nowait = true,
+                remap = false,
+            },
+            { "<leader>r",   desc = "Run ipython cell like in VSCode",          nowait = true, remap = false },
+            { "<leader>t",   desc = "Open buffer in new tab",                   nowait = true, remap = false },
+            { "<leader>x",   group = "Encodings, endings and so on",            nowait = true, remap = false },
+            { "<leader>xc",  group = "Set file encoding",                       nowait = true, remap = false },
+            { "<leader>xcu", desc = "utf-8",                                    nowait = true, remap = false },
+            { "<leader>xe",  group = "Set line endings",                        nowait = true, remap = false },
+            { "<leader>xed", desc = "Dos style",                                nowait = true, remap = false },
+            { "<leader>xeu", desc = "Unix style",                               nowait = true, remap = false },
+            { "<leader>xh",  group = "HEX editing",                             nowait = true, remap = false },
+            { "<leader>xhh", desc = "Display as HEX",                           nowait = true, remap = false },
+            { "<leader>xhr", desc = "Convert from HEX",                         nowait = true, remap = false },
+            { "<leader>xo",  group = "Reopen with encodings or in binary mode", nowait = true, remap = false },
+            { "<leader>xob", desc = "binary",                                   nowait = true, remap = false },
+            { "<leader>xoc", desc = "cp1251",                                   nowait = true, remap = false },
+            { "<leader>xot", desc = "text (default mode)",                      nowait = true, remap = false },
+            { "<leader>xs",  group = "Spelling",                                nowait = true, remap = false },
+            { "<leader>xse", desc = "en",                                       nowait = true, remap = false },
+            { "<leader>xsn", desc = "off",                                      nowait = true, remap = false },
+            { "<leader>xsr", desc = "ru",                                       nowait = true, remap = false },
         },
-
-        h = {
-            name = "Harpoon",
-            m = { "Set marks" },
-            u = { "Open files quick menu" },
-            c = { "Open term commands quick menu" },
-            n = { "Go to the next mark" },
-            p = { "Go to the previous mark" },
-        },
-
-        l = {
-            name = "LSP",
-            f = { "<CMD>lua vim.lsp.buf.format({async=true})<CR>", "Formatting code" },
-            r = { "<CMD>lua vim.lsp.buf.rename()<CR>", "Rename" },
-            a = { "<CMD>lua vim.lsp.buf.code_action()<CR>", "Code action" },
-            j = { "<CMD>lua vim.diagnostic.goto_next({buffer=0})<CR>", "Go to next diagnostic" },
-            k = { "<CMD>lua vim.diagnostic.goto_prev({buffer=0})<CR>", "Go to prev diagnostic" },
-            q = { "<CMD>lua vim.diagnostic.setloclist()<CR>", "Diagnostic list" },
-            s = { "<CMD>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
-            i = { "<CMD>LspInfo<CR>", "Lsp Info" },
-        },
-
-        g = {
-            name = "Git",
-            g = { "Lazygit" },
-            j = { "Next Hunk" },
-            k = { "Prev Hunk" },
-            l = { "Blame" },
-            p = { "Preview Hunk" },
-            r = { "Reset Hunk" },
-            R = { "Reset Buffer" },
-            s = { "Stage Hunk" },
-            u = { "Undo Stage Hunk" },
-            o = { "Open changed file" },
-            b = { "Checkout branch" },
-            c = { "Checkout commit" },
-            d = { "Diff" },
-        },
-
-        d = {
-            name = "DAP",
-            b = { "Toggle breakpoint" },
-            B = { "Conditional breakpoint" },
-            H = { "Hit condition breakpoint" },
-            L = { "Log point" },
-            c = { "Continue <F5>" },
-            i = { "Step into <F2>" },
-            o = { "Step over <F3>" },
-            O = { "Step out <F4>" },
-            r = { "Toggle repl" },
-            l = { "Run last" },
-            u = { "Toggle UI" },
-            q = { "Quit (terminate) session" },
-            s = { "Debug selected" },
-            t = {
-                name = "Debug tests",
-                c = { "Class under cursor" },
-                m = { "Method under cursor" },
-            },
-        },
-
-        x = {
-            name = "Encodings, endings and so on",
-            o = {
-                name = "Reopen with encodings or in binary mode",
-                c = { "cp1251" },
-                b = { "binary" },
-                t = { "text (default mode)" },
-            },
-            c = {
-                name = "Set file encoding",
-                u = { "utf-8" },
-            },
-            s = {
-                name = "Spelling",
-                e = { "en" },
-                r = { "ru" },
-                n = { "off" },
-            },
-            e = {
-                name = "Set line endings",
-                u = { "Unix style" },
-                d = { "Dos style" },
-            },
-            h = {
-                name = "HEX editing",
-                h = { "Display as HEX" },
-                r = { "Convert from HEX" },
-            },
-        },
-
-        --[[ t = { ]]
-        --[[   name = "Terminal", ]]
-        --[[   n = { "<CMD>lua _NODE_TOGGLE()<CR>", "Node" }, ]]
-        --[[   u = { "<CMD>lua _NCDU_TOGGLE()<CR>", "NCDU" }, ]]
-        --[[   t = { "<CMD>lua _HTOP_TOGGLE()<CR>", "Htop" }, ]]
-        --[[   p = { "<CMD>lua _PYTHON_TOGGLE()<CR>", "Python" }, ]]
-        --[[   f = { "<CMD>ToggleTerm direction=float<CR>", "Float" }, ]]
-        --[[   h = { "<CMD>ToggleTerm size=10 direction=horizontal<CR>", "Horizontal" }, ]]
-        --[[   v = { "<CMD>ToggleTerm size=80 direction=vertical<CR>", "Vertical" }, ]]
-        --[[ }, ]]
-    }
+    })
 
     which_key.setup(setup)
-    which_key.register(mappings, opts)
 end
 
 return M
