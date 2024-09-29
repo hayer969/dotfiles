9ddd51c0882c0ffedf9ffd0b50f2ba0aaeadf2c8
hayer969 <hayerdp@gmail.com>
2024-09-29 19:56:06 +0300

    Update markdown preview settings for using inside container

--- .config/nvim/lua/user/markdown.lua
+++ .config/nvim/lua/user/markdown.lua
@@ -1,7 +1,27 @@
 local M = {
-    "iamcco/markdown-preview.nvim",
+	"iamcco/markdown-preview.nvim",
 }
 
-function M.config() end
+function M.config()
+	function file_exists(name)
+		local f = io.open(name, "r")
+		if f ~= nil then
+			io.close(f)
+			return true
+		else
+			return false
+		end
+	end
+
+	if file_exists("/run/.containerenv") then
+		vim.cmd([[
+        function OpenMarkdownPreview (url)
+            let cmd = "flatpak-spawn --host firefox --new-window " . shellescape(a:url) . " &"
+            silent call system(cmd)
+          endfunction
+        ]])
+		vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+	end
+end
 
 return M
