955cad153a4237875d4bc447d9841af6896d641b
Dmitry Prytkov <dmitry.prytkov@flant.com>
2024-11-26 14:55:53 +0300

    Schemas for yaml files, start with Taskconfig schema

---
+++ .config/nvim/lua/user/lspsettings/yamlls.lua
@@ -0,0 +1,9 @@
+return {
+    settings = {
+        yaml = {
+            schemas = {
+                ["https://taskfile.dev/schema.json"] = {"/task*.yml", "/Task*.yml"},
+            },
+        },
+    },
+}
