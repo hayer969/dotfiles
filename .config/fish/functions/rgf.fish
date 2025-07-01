12b1ece3a8760e9bfda93eb0c34bd0f71049dd90
hayer969 <hayerdp@gmail.com>
2025-07-02 00:57:00 +0300

    Fish function for open greped files in nvim

---
+++ .config/fish/functions/rgf.fish
@@ -0,0 +1,26 @@
+function rgf
+    if command -q rg; and command -q fzf; and command -q nvim; and command -q bat
+        set -l list (
+            rg --no-heading \
+               --line-number $argv | \
+               fzf --delimiter : \
+                   --preview 'bat \
+                              --style=full \
+                              --color=always \
+                              --highlight-line {2} \
+                              --line-range {2}:-5 \
+                              --line-range {2}: {1}' | \
+               cut -d: -f1,2 | \
+               sed 's/:/:+/'
+        )
+        set -l first (echo $list[1] | string split :)
+        set --erase list[1]
+        set -l list2
+        for el in $list
+            set --append list2 (echo $el | sed 's/:.*//')
+        end
+        nvim $first $list2
+    else
+        echo "Please install rg, fzf, nvim, bat"
+    end
+end
