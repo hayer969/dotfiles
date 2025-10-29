ffe991b7771747a3e8e055770af08e661a300a21
Dmitry Prytkov <dmitry.prytkov@flant.com>
2025-10-29 19:42:04 +0300

    Improve fish functions for working with gh

--- .config/fish/functions/ghactionstatus.fish
+++ .config/fish/functions/ghactionstatus.fish
@@ -1,19 +1,18 @@
 function ghactionstatus --description 'Check github ci action status for particular pr. By default - build images for deckhouse/virtualization'
-    argparse --min-args=1 'h/help' 'build' 'docs' 'repo=' 'action=' -- $argv
+    argparse --min-args=1 'h/help' 'repo=' 'action=+' -- $argv
     or return
     if not command -q gh
         echo "Error. Please install 'gh' command (github cli)" >&2
         return 1
     end
     if set -ql _flag_h
-        echo "Usage: ghactionstatus [--help] [--docs] [--repo=owner/repo_name] [--action=action_name] pr_number"
-        echo "-h/--help - show this help"
-        echo "--docs - equal to --action='Validation doc-changes'"
-        echo "--repo= - repository owner and name"
-        echo "--action= - full action name"
-        echo "pr_number - mandatory option, just pr number"
-        echo "defaults: --repo='deckhouse/virtualization' --action='Build and Push images'"
-        return
+        echo "Usage: ghactionstatus [--help] [--repo=owner/repo_name] [--action=action_name] pr_number" >&2
+        echo "-h/--help - show this help" >&2
+        echo "--repo= - repository owner and name" >&2
+        echo "--action=+ - full action name" >&2
+        echo "pr_number - mandatory option, just pr number" >&2
+        echo "defaults: --repo='deckhouse/virtualization' --action='Build and Push images' --action='Validation doc-changes'" >&2
+        return 1
     end
 
     set --local pr_number "$argv[1]"
@@ -22,14 +21,12 @@ function ghactionstatus --description 'Check github ci action status for particu
     set -ql _flag_repo[1]
     and set --local owner_repo $_flag_repo[-1]
 
-    set --local action_name "Build and Push images";
-    set -ql _flag_docs
-    and set --local action_name "Validation doc-changes"
-
+    set --local action_names "Build and Push images" "Validation doc-changes";
     set -ql _flag_action[1]
-    and set --local action_name $_flag_action[-1]
-
+    and set --local action_names $_flag_action
 
     set --local commit "$(gh pr view $pr_number --repo $owner_repo --json commits --jq ".commits[-1].oid")"
-    gh api repos/$owner_repo/commits/$commit/check-runs --jq ".check_runs[] | select(.name==\"$action_name\") | {name: .name, status: .status, conclusion: .conclusion, completed_at: .completed_at}"
+    for action in $action_names
+        gh api repos/$owner_repo/commits/$commit/check-runs --jq ".check_runs[] | select(.name==\"$action\") | {name: .name, status: .status, conclusion: .conclusion, completed_at: .completed_at}"
+    end
 end
